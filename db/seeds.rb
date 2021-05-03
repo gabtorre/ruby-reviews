fe9f515f91e0d3026ba198c77f57437c645985fa
gabtorre <gtorresx3@gmail.com>
2021-05-03 13:24:54 -0700

    Add db and schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+places = Place.create([
+    {
+        name: 'Place 1',
+        image_url: 'https://picsum.photos/200'
+    }
+])
+
+reviews = Review.create([
+    {
+        title: 'Great Place', 
+        description: 'my new fav place',
+        score: 5,
+        place: places.first
+    }
+])
