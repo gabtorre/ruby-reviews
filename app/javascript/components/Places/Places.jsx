6827de2ec4645632e893fd0504fb5138eb7f7fb3
gabtorre <gtorresx3@gmail.com>
2021-05-04 12:26:14 -0700

    Add Styled Components

--- app/javascript/components/Places/Places.jsx
+++ app/javascript/components/Places/Places.jsx
@@ -1,6 +1,26 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Place from './Place'
+import styled from 'styled-components'
+
+const Home = styled.div`
+    text-align: center;
+    max-width: 1200px;
+    margin-left: auto;
+    margin-right: auto;
+`
+const Header = styled.div`
+    padding: 100px 100px 10px 100px;
+
+    h1 {
+        font-size: 42px;
+    }
+`
+const Subheader = styled.div`
+    font-weight: 300;
+    font-size: 26px;
+`
+const Grid = styled.div``
 
 const Places = () => {
     const [places, setPlaces] = useState([])
@@ -23,15 +43,15 @@ const Places = () => {
     })
 
     return (
-        <div className="home">
-            <div className="header">
+        <Home>
+            <Header>
                 <h1>Streats</h1>
-                <div className="subheader">The best street food.</div>
-            </div>
+                <Subheader>The best street food.</Subheader>
+            </Header>
             <div className="grid">
                 {grid}
             </div>
-        </div>
+        </Home>
     )
 }
 
