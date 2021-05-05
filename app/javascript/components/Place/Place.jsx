90e2bf497676794ac4fa1e06c8476de97f776d27
gabtorre <gtorresx3@gmail.com>
2021-05-04 22:35:08 -0700

    Add Review Form

--- app/javascript/components/Place/Place.jsx
+++ app/javascript/components/Place/Place.jsx
@@ -1,14 +1,100 @@
 import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import Header from './Header'
+import styled from 'styled-components'
+import ReviewForm from './ReviewForm'
+
+const Wrapper = styled.div`
+    margin-left: auto;
+    margin-right: auto;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+`
+const Column = styled.div`
+    background: #fff;
+    height: 100vh;
+    overflow: scroll;
+
+    &:last-child {
+        background: #000;
+    }
+`
+const Main = styled.div`
+    padding-left: 50px;
+`
+
 
 const Place = (props) => {
     const [place, setPlace] = useState({})
     const [review, setReview] = useState({})
+    const [loaded, setLoaded] = useState(false)
 
     useEffect(() => {
-        
+        const slug = props.match.params.slug
+        const url = `/api/v1/places/${slug}`
+
+        axios.get(url)
+        .then(resp => {
+            setPlace(resp.data)
+            setLoaded(true)
+        })
+        .catch(resp => console.log(resp))
     }, [])
 
-    return <div>Place show view</div>
+    const handleChange = (e) => {
+        e.preventDefault()
+        
+        setReview(Object.assign({}, review, {[e.target.name]: e.target.value}))
+
+        console.log(review)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        const csrfToken = document.querySelector('[name=csrf-token]').content
+        axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken
+
+        const place_id = place.data.id
+        axios.post('/api/v1/reviews', {review, place_id})
+        .then( resp => {
+            const included = [...place.included, resp.data]
+            setPlace({...place, included})
+            setReview({title: '', description: '', score: 0})
+        } )
+        .catch( resp => {} )
+    }
+
+    const setRating = (score, e) => {
+        setReview({...review, score})
+    }
+
+    return (
+        <Wrapper>
+            {loaded &&
+                <>
+                    <Column>
+                        <Main>
+                                <Header 
+                                    attributes={place.data.attributes}
+                                    reviews={place.included}
+                                />
+                            <div className="reviews"></div>
+                        </Main>
+                    </Column>
+                    <Column>
+                        <ReviewForm
+                            handleChange={handleChange}
+                            handleSubmit={handleSubmit}
+                            setRating={setRating}
+                            attributes={place.data.attributes}
+                            review={review}
+                        />
+                    </Column>
+                </>
+            }
+        </Wrapper>
+    )
 }
 
 export default Place
