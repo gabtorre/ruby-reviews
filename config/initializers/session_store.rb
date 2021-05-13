9698341cd5a9fdb936e7d69d2e87fa4f57e72f6d
gabtorre <gtorresx3@gmail.com>
2021-05-13 11:55:45 -0700

    Add Registration Controller

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: "_auth", domain: "streats-api-21.herokuapp.com"
+if Rails.env == "production"
+    Rails.application.config.session_store :cookie_store, key: "_auth", domain: "streats-api-21.herokuapp.com"
+else 
+    Rails.application.config.session_store :cookie_store, key: "_auth"
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
 
-  
   root 'pages#index'
   
   namespace :api do
     namespace :v1 do
       resources :sessions, only: [:create]
+      resources :registrations, only: [:create]
+      delete :logout, to: "sessions#logout"
+      get :logged_in, to: "sessions#logged_in"
       resources :places, param: :slug
       resources :reviews, only: [:create, :destroy]
     end
